30d854e808b20ec8c1f2b557df2a1f04c12935e8
pvslabs <pvslaboratorieslimited@gmail.com>
2025-05-13 17:52:04 +0530

    logo update and links update in index

--- assets/js/pre-load.js
+++ assets/js/pre-load.js
@@ -18,5 +18,5 @@ document.addEventListener('DOMContentLoaded', function() {
           document.body.classList.add('loaded');
           preloader.remove();
       }
-  }, 5000); // Updated to 5 seconds as per the comment
+  }, 1000); // Updated to 5 seconds as per the comment
 });
